bbfe5a1e05dc4c86c216357ae67d9bec1b3fa068
keanson <49366089+keanson@users.noreply.github.com>
2023-05-22 15:14:58 +0800

    Add files via upload

--- io_utils.py
+++ io_utils.py
@@ -12,7 +12,9 @@ model_dict = dict(
             ResNet18 = backbone.ResNet18,
             ResNet34 = backbone.ResNet34,
             ResNet50 = backbone.ResNet50,
-            ResNet101 = backbone.ResNet101)
+            ResNet101 = backbone.ResNet101,
+            ResNet50pretrained = backbone.ResNet50pretrained,
+            ResNet18pretrained = backbone.ResNet18pretrained)
 
 def parse_args(script):
     parser = argparse.ArgumentParser(description= 'few-shot script %s' %(script))
@@ -24,7 +26,13 @@ def parse_args(script):
     parser.add_argument('--test_n_way'  , default=5, type=int,  help='class num to classify for testing (validation) ') #baseline and baseline++ only use this parameter in finetuning
     parser.add_argument('--n_shot'      , default=5, type=int,  help='number of labeled data in each class, same as n_support') #baseline and baseline++ only use this parameter in finetuning
     parser.add_argument('--train_aug'   , action='store_true',  help='perform data augmentation or not during training ') #still required for save_features.py and test.py to find the model path correctly
-
+    parser.add_argument('--steps'   , default=2, type=int,  help='mean field steps for cdkt') 
+    parser.add_argument('--tau'   , default=1., type=float,  help='temperature for cdkt')
+    parser.add_argument('--loss'   , default='ELBO', type=str,  help='loss for cdkt')
+    parser.add_argument('--mean'   , default=0., type=float,  help='prior mean for cdkt, should be negative if we want to be like softmax')
+    parser.add_argument('--kernel'   , default='bncossim', type=str,  help='kernel for GP, choices include: linear, rbf, spectral (regression only), matern, poli1, poli2, cossim, bncossim')
+    parser.add_argument('--batch_size'   , default=1, type=int,  help='task numbers in parallel for cdkt') 
+    
     if script == 'train':
         parser.add_argument('--num_classes' , default=200, type=int, help='total number of classes in softmax, only used in baseline') #make it larger than the maximum label value in base class
         parser.add_argument('--save_freq'   , default=50, type=int, help='Save frequency')
