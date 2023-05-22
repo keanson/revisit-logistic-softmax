1eb43a8af439fc8b4c93e10dd1fa7da0d99f3472
keanson <49366089+keanson@users.noreply.github.com>
2023-05-22 15:24:39 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,6 +2,4 @@ save_dir                    = './save/'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/'
 data_dir['miniImagenet']    = './filelists/miniImagenet/'
-data_dir['omniglot']        = './filelists/omniglot/'
-data_dir['emnist']          = './filelists/emnist/'
 kernel_type                 = 'bncossim' #linear, rbf, spectral (regression only), matern, poli1, poli2, cossim, bncossim 
