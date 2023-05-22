c9b896f04b96b292ad6d6a72714845b40842ee47
keanson <49366089+keanson@users.noreply.github.com>
2023-05-22 15:08:58 +0800

    Update download_CUB.sh

--- filelists/CUB/download_CUB.sh
+++ filelists/CUB/download_CUB.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+wget https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1
 tar -zxvf CUB_200_2011.tgz
 python write_CUB_filelist.py
